0781fbf79c70e01d7a50c311e662299c336fd5dd
Jeremy (Pololu) <dev-jeremy@pololu.com>
2017-11-03 12:44:19 -0700

    DualG2HighPowerMotorShield: added example demo program.

---
+++ examples/Demo/Demo.ino
@@ -0,0 +1,114 @@
+#include "DualG2HighPowerMotorShield.h"
+
+// Uncomment the version corresponding with the version of your shield. 
+DualG2HighPowerMotorShield18v md;
+// DualG2HighPowerMotorShield24v md; 
+
+void stopIfFault()
+{
+  if (md.getM1Fault())
+  {
+    Serial.println("M1 fault");
+    while(1);
+  }
+  if (md.getM2Fault())
+  {
+    Serial.println("M2 fault");
+    while(1);
+  }
+}
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println("Dual G2 High Power Motor Shield");
+  md.init();
+  
+  delay(10);
+  
+  // Uncomment to flip a motor's direction:
+  //md.flipM1(true);
+  //md.flipM2(true);
+}
+
+void loop()
+{
+  md.enableDrivers();
+  delay(1);  // The drivers require a maximum of 1ms to elapse when brought out of sleep mode.
+	
+  for (int i = 0; i <= 400; i++)
+  {
+    md.setM1Speed(i);
+    stopIfFault();
+    if (i%200 == 100)
+    {
+      Serial.print("M1 current: ");
+      Serial.println(md.getM1CurrentMilliamps());
+    }
+    delay(2);
+  }
+  
+  for (int i = 400; i >= -400; i--)
+  {
+    md.setM1Speed(i);
+    stopIfFault();
+    if (i%200 == 100)
+    {
+      Serial.print("M1 current: ");
+      Serial.println(md.getM1CurrentMilliamps());
+    }
+    delay(2);
+  }
+  
+  for (int i = -400; i <= 0; i++)
+  {
+    md.setM1Speed(i);
+    stopIfFault();
+    if (i%200 == 100)
+    {
+      Serial.print("M1 current: ");
+      Serial.println(md.getM1CurrentMilliamps());
+    }
+    delay(2);
+  }
+
+  for (int i = 0; i <= 400; i++)
+  {
+    md.setM2Speed(i);
+    stopIfFault();
+    if (i%200 == 100)
+    {
+      Serial.print("M2 current: ");
+      Serial.println(md.getM2CurrentMilliamps());
+    }
+    delay(2);
+  }
+  
+  for (int i = 400; i >= -400; i--)
+  {
+    md.setM2Speed(i);
+    stopIfFault();
+    if (i%200 == 100)
+    {
+      Serial.print("M2 current: ");
+      Serial.println(md.getM2CurrentMilliamps());
+    }
+    delay(2);
+  }
+  
+  for (int i = -400; i <= 0; i++)
+  {
+    md.setM2Speed(i);
+    stopIfFault();
+    if (i%200 == 100)
+    {
+      Serial.print("M2 current: ");
+      Serial.println(md.getM2CurrentMilliamps());
+    }
+    delay(2);
+  }
+  
+  md.disableDrivers(); // Put the MOSFET drivers into sleep mode. 
+  delay(500);
+  
+}
