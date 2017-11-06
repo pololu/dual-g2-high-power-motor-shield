fde07eafdd46a66311e9099afe10fe9efd757f88
Jeremy (Pololu) <dev-jeremy@pololu.com>
2017-11-06 12:38:23 -0800

    DualG2HighPowerMotorShield: still problems with functions using current offset.

--- DualG2HighPowerMotorShield.cpp
+++ DualG2HighPowerMotorShield.cpp
@@ -10,16 +10,16 @@ DualG2HighPowerMotorShield::DualG2HighPowerMotorShield()
   _M1nSLEEP = 2;
   _M1nFAULT = 6;
   _M1DIR = 7;
-  _M1CS = A0; 
-  
+  _M1CS = A0;
+
   _M2nSLEEP = 4;
   _M2nFAULT = 12;
   _M2DIR = 8;
   _M2CS = A1;
 }
 
-DualG2HighPowerMotorShield::DualG2HighPowerMotorShield(	unsigned char M1nSLEEP, unsigned char M1DIR, unsigned char M1nFAULT, unsigned char M1CS, 
-														unsigned char M2nSLEEP, unsigned char M2DIR, unsigned char M2nFAULT, unsigned char M2CS) 
+DualG2HighPowerMotorShield::DualG2HighPowerMotorShield(	unsigned char M1nSLEEP, unsigned char M1DIR, unsigned char M1nFAULT, unsigned char M1CS,
+    unsigned char M2nSLEEP, unsigned char M2DIR, unsigned char M2nFAULT, unsigned char M2CS)
 {
   //Pin map
   //PWM1 and PWM2 cannot be remapped because the library assumes PWM is on timer1
@@ -27,7 +27,7 @@ DualG2HighPowerMotorShield::DualG2HighPowerMotorShield(	unsigned char M1nSLEEP, 
   _M1nFAULT = M1nFAULT;
   _M1DIR = M1DIR;
   _M1CS = M1CS;
-  
+
   _M2nSLEEP = M2nSLEEP;
   _M2nFAULT = M2nFAULT;
   _M2DIR = M2DIR;
@@ -37,20 +37,20 @@ DualG2HighPowerMotorShield::DualG2HighPowerMotorShield(	unsigned char M1nSLEEP, 
 // Public Methods //////////////////////////////////////////////////////////////
 void DualG2HighPowerMotorShield::init()
 {
-// Define pinMode for the pins and set the frequency for timer1.
+  // Define pinMode for the pins and set the frequency for timer1.
 
-  pinMode(_M1nSLEEP,OUTPUT);
-  pinMode(_M2nSLEEP,OUTPUT);
-  pinMode(_M1PWM,OUTPUT);
-  pinMode(_M1nFAULT,INPUT_PULLUP);
-  pinMode(_M1CS,INPUT);
-  pinMode(_M1DIR,OUTPUT);
-  pinMode(_M2DIR,OUTPUT);
-  pinMode(_M2PWM,OUTPUT);
-  pinMode(_M2nFAULT,INPUT_PULLUP);
-  pinMode(_M2CS,INPUT);
-    
-  #if defined(__AVR_ATmega168__)|| defined(__AVR_ATmega328P__) || defined(__AVR_ATmega32U4__)
+  pinMode(_M1nSLEEP, OUTPUT);
+  pinMode(_M2nSLEEP, OUTPUT);
+  pinMode(_M1PWM, OUTPUT);
+  pinMode(_M1nFAULT, INPUT_PULLUP);
+  pinMode(_M1CS, INPUT);
+  pinMode(_M1DIR, OUTPUT);
+  pinMode(_M2DIR, OUTPUT);
+  pinMode(_M2PWM, OUTPUT);
+  pinMode(_M2nFAULT, INPUT_PULLUP);
+  pinMode(_M2CS, INPUT);
+
+#if defined(__AVR_ATmega168__)|| defined(__AVR_ATmega328P__) || defined(__AVR_ATmega32U4__)
   // Timer 1 configuration
   // prescaler: clockI/O / 1
   // outputs enabled
@@ -62,13 +62,13 @@ void DualG2HighPowerMotorShield::init()
   TCCR1A = 0b10100000;
   TCCR1B = 0b00010001;
   ICR1 = 400;
-  #endif
+#endif
 }
 // Set speed for motor 1, speed is a number betwenn -400 and 400
 void DualG2HighPowerMotorShield::setM1Speed(int speed)
 {
   unsigned char reverse = 0;
-  
+
   if (speed < 0)
   {
     speed = -speed;  // Make speed a positive quantity
@@ -76,20 +76,20 @@ void DualG2HighPowerMotorShield::setM1Speed(int speed)
   }
   if (speed > 400)  // Max PWM dutycycle
     speed = 400;
-	
-  #if defined(__AVR_ATmega168__)|| defined(__AVR_ATmega328P__) || defined(__AVR_ATmega32U4__)
+
+#if defined(__AVR_ATmega168__)|| defined(__AVR_ATmega328P__) || defined(__AVR_ATmega32U4__)
   OCR1A = speed;
-  #else
-  analogWrite(_M1PWM,speed * 51 / 80); // default to using analogWrite, mapping 400 to 255
-  #endif
-  
+#else
+  analogWrite(_M1PWM, speed * 51 / 80); // default to using analogWrite, mapping 400 to 255
+#endif
+
   if (reverse ^ _flipM1) // flip if speed was negative or _flipM1 setting is active, but not both
   {
-    digitalWrite(_M1DIR,HIGH);
+    digitalWrite(_M1DIR, HIGH);
   }
   else
   {
-    digitalWrite(_M1DIR,LOW);
+    digitalWrite(_M1DIR, LOW);
   }
 }
 
@@ -97,28 +97,28 @@ void DualG2HighPowerMotorShield::setM1Speed(int speed)
 void DualG2HighPowerMotorShield::setM2Speed(int speed)
 {
   unsigned char reverse = 0;
-  
+
   if (speed < 0)
   {
     speed = -speed;  // make speed a positive quantity
     reverse = 1;  // preserve the direction
   }
-  if (speed > 400)  // Max 
+  if (speed > 400)  // Max
     speed = 400;
-	
-  #if defined(__AVR_ATmega168__)|| defined(__AVR_ATmega328P__) || defined(__AVR_ATmega32U4__)
+
+#if defined(__AVR_ATmega168__)|| defined(__AVR_ATmega328P__) || defined(__AVR_ATmega32U4__)
   OCR1B = speed;
-  #else
-  analogWrite(_M2PWM,speed * 51 / 80); // default to using analogWrite, mapping 400 to 255
-  #endif 
-  
+#else
+  analogWrite(_M2PWM, speed * 51 / 80); // default to using analogWrite, mapping 400 to 255
+#endif
+
   if (reverse ^ _flipM2) // flip if speed was negative or _flipM1 setting is active, but not both
   {
-    digitalWrite(_M2DIR,HIGH);
+    digitalWrite(_M2DIR, HIGH);
   }
   else
   {
-    digitalWrite(_M2DIR,LOW);
+    digitalWrite(_M2DIR, LOW);
   }
 }
 
@@ -129,13 +129,13 @@ void DualG2HighPowerMotorShield::setSpeeds(int m1Speed, int m2Speed)
   setM2Speed(m2Speed);
 }
 
-// Return error status for motor 1 
+// Return error status for motor 1
 unsigned char DualG2HighPowerMotorShield::getM1Fault()
 {
   return !digitalRead(_M1nFAULT);
 }
 
-// Return error status for motor 2 
+// Return error status for motor 2
 unsigned char DualG2HighPowerMotorShield::getM2Fault()
 {
   return !digitalRead(_M2nFAULT);
@@ -151,92 +151,93 @@ void DualG2HighPowerMotorShield::flipM2(boolean flip)
   DualG2HighPowerMotorShield::_flipM2 = flip;
 }
 
-signed int DualG2HighPowerMotorShield::getM1CurrentReading()
+unsigned int DualG2HighPowerMotorShield::getM1CurrentReading()
 {
-	return analogRead(_M1CS);
+  return analogRead(_M1CS);
 }
 
-signed int DualG2HighPowerMotorShield::getM2CurrentReading()
+unsigned int DualG2HighPowerMotorShield::getM2CurrentReading()
 {
-	return analogRead(_M2CS);
+  return analogRead(_M2CS);
 }
 
-// Enables the MOSFET driver for M1. 
+// Enables the MOSFET driver for M1.
 void DualG2HighPowerMotorShield::enableM1Driver()
 {
-	digitalWrite(_M1nSLEEP, HIGH);
+  digitalWrite(_M1nSLEEP, HIGH);
 }
 
-// Enables the MOSFET driver for M2. 
+// Enables the MOSFET driver for M2.
 void DualG2HighPowerMotorShield::enableM2Driver()
 {
-	digitalWrite(_M2nSLEEP, HIGH);
+  digitalWrite(_M2nSLEEP, HIGH);
 }
 
-// Enables the MOSFET drivers for both M1 and M2. 
+// Enables the MOSFET drivers for both M1 and M2.
 void DualG2HighPowerMotorShield::enableDrivers()
 {
-	enableM1Driver();
-	enableM2Driver();
+  enableM1Driver();
+  enableM2Driver();
 }
 
 // Puts the MOSFET driver for M1 into sleep mode.
 void DualG2HighPowerMotorShield::disableM1Driver()
 {
-	digitalWrite(_M1nSLEEP, LOW);
+  digitalWrite(_M1nSLEEP, LOW);
 }
 
 // Puts the MOSFET driver for M2 into sleep mode.
 void DualG2HighPowerMotorShield::disableM2Driver()
 {
-	digitalWrite(_M2nSLEEP, LOW);
+  digitalWrite(_M2nSLEEP, LOW);
 }
 
-// Puts the MOSFET drivers for both M1 and M2 into sleep mode. 
+// Puts the MOSFET drivers for both M1 and M2 into sleep mode.
 void DualG2HighPowerMotorShield::disableDrivers()
 {
-	disableM1Driver();
-	disableM2Driver();
+  disableM1Driver();
+  disableM2Driver();
 }
 
 
 
-// Set voltage offset of M1 current reading at 0 speed. 
+// Set voltage offset of M1 current reading at 0 speed.
 void DualG2HighPowerMotorShield::calibrateM1CurrentOffset()
 {
-	setM1Speed(0);
-	enableM1Driver();
-	delay(1);
-	_offsetM1 = getM1CurrentReading;
+  setM1Speed(0);
+  enableM1Driver();
+  delay(1);
+  DualG2HighPowerMotorShield::_offsetM1 = getM1CurrentReading();
+
 }
 
-// Set voltage offset of M2 current reading at 0 speed. 
+// Set voltage offset of M2 current reading at 0 speed.
 void DualG2HighPowerMotorShield::calibrateM2CurrentOffset()
 {
-	setM2Speed(0);
-	enableM2Driver();
-	delay(1);
-	_offsetM2 = getM2CurrentReading;
+  setM2Speed(0);
+  enableM2Driver();
+  delay(1);
+  DualG2HighPowerMotorShield::_offsetM2 = getM2CurrentReading();
 }
 
 // Get voltage offset of M1 and M2 current readings.
 void DualG2HighPowerMotorShield::calibrateCurrentOffsets()
 {
-	setSpeeds( 0, 0);
-	enableDrivers();
-	delay(1);
-	_offsetM1 = getM1CurrentReading();
-	_offsetM2 = getM2CurrentReading();
+  setSpeeds( 0, 0);
+  enableDrivers();
+  delay(1);
+  DualG2HighPowerMotorShield::_offsetM1 = getM1CurrentReading();
+  DualG2HighPowerMotorShield::_offsetM2 = getM2CurrentReading();
 }
 
 // Return M1 current value in milliamps for 18V version.
 unsigned int DualG2HighPowerMotorShield18v::getM1CurrentMilliamps()
 {
   // 5V / 1024 ADC counts / 10 mV per A = 488 mA per count
-  int reading = (getM1CurrentReading() - _offsetM1) ;
+  int reading = (getM1CurrentReading() - DualG2HighPowerMotorShield::_offsetM1) ;
   if (reading > 0)
   {
-	  return reading * 488;
+    return reading * 488;
   }
   return 0;
 }
@@ -245,10 +246,10 @@ unsigned int DualG2HighPowerMotorShield18v::getM1CurrentMilliamps()
 unsigned int DualG2HighPowerMotorShield18v::getM2CurrentMilliamps()
 {
   // 5V / 1024 ADC counts / 10 mV per A = 488 mA per count
-  int reading = (getM2CurrentReading() - _offsetM2);
+  int reading = (getM2CurrentReading() - DualG2HighPowerMotorShield::_offsetM2);
   if (reading > 0)
   {
-	  return reading * 488;
+    return reading * 488;
   }
   return 0;
 }
@@ -257,11 +258,11 @@ unsigned int DualG2HighPowerMotorShield18v::getM2CurrentMilliamps()
 unsigned int DualG2HighPowerMotorShield24v::getM1CurrentMilliamps()
 {
   // 5V / 1024 ADC counts / 20 mV per A = 244 mA per count
-  // there is an approximately 50mA offset 
-  int reading = (getM1CurrentReading() - _offsetM1);
+  // there is an approximately 50mA offset
+  int reading = (getM1CurrentReading() - DualG2HighPowerMotorShield::_offsetM1);
   if (reading > 0)
   {
-	  return reading * 244;
+    return reading * 244;
   }
   return 0;
 }
@@ -270,13 +271,12 @@ unsigned int DualG2HighPowerMotorShield24v::getM1CurrentMilliamps()
 unsigned int DualG2HighPowerMotorShield24v::getM2CurrentMilliamps()
 {
   // 5V / 1024 ADC counts / 20 mV per A = 244 mA per count
-  // there is an approximately 50mA offset 
-  int reading = (getM2CurrentReading() - _offsetM2) ;
+  // there is an approximately 50mA offset
+  int reading = (getM2CurrentReading() - DualG2HighPowerMotorShield::_offsetM2) ;
   if (reading > 0)
   {
-	  return reading * 244;
+    return reading * 244;
   }
   return 0;
 }
-
 
--- DualG2HighPowerMotorShield.h
+++ DualG2HighPowerMotorShield.h
@@ -5,72 +5,72 @@
 
 class DualG2HighPowerMotorShield
 {
-  public:  
+  public:
     // CONSTRUCTORS
     DualG2HighPowerMotorShield(); // Default pin selection.
-    DualG2HighPowerMotorShield(	unsigned char M1nSLEEP, unsigned char M1DIR, unsigned char M1nFAULT, unsigned char M1CS, 
-								unsigned char M2nSLEEP, unsigned char M2DIR, unsigned char M2nFAULT, unsigned char M2CS); // User-defined pin selection. 
-    
+    DualG2HighPowerMotorShield(	unsigned char M1nSLEEP, unsigned char M1DIR, unsigned char M1nFAULT, unsigned char M1CS,
+                                unsigned char M2nSLEEP, unsigned char M2DIR, unsigned char M2nFAULT, unsigned char M2CS); // User-defined pin selection.
+
     // PUBLIC METHODS
-    void init(); // Initialize TIMER 1, set the PWM to 20kHZ. 
+    void init(); // Initialize TIMER 1, set the PWM to 20kHZ.
     void setM1Speed(int speed); // Set speed for M1.
     void setM2Speed(int speed); // Set speed for M2.
     void setSpeeds(int m1Speed, int m2Speed); // Set speed for both M1 and M2.
     unsigned char getM1Fault(); // Get fault reading from M1.
     unsigned char getM2Fault(); // Get fault reading from M2.
-	void flipM1(boolean flip); // Flip the direction of the speed for M1.
+    void flipM1(boolean flip); // Flip the direction of the speed for M1.
     void flipM2(boolean flip); // Flip the direction of the speed for M2.
-	void enableM1Driver(); // Enables the MOSFET driver for M1. 
-	void enableM2Driver(); // Enables the MOSFET driver for M2. 
-	void enableDrivers(); // Enables the MOSFET drivers for both M1 and M2. 
-	void disableM1Driver(); // Puts the MOSFET driver for M1 into sleep mode.
-	void disableM2Driver(); // Puts the MOSFET driver for M2 into sleep mode.
-	void disableDrivers(); // Puts the MOSFET drivers for both M1 and M2 into sleep mode. 
-	signed int getM1CurrentReading();
-	signed int getM2CurrentReading();
-	void calibrateM1CurrentOffset();
-	void calibrateM2CurrentOffset();
-	void calibrateCurrentOffsets();
-    
+    void enableM1Driver(); // Enables the MOSFET driver for M1.
+    void enableM2Driver(); // Enables the MOSFET driver for M2.
+    void enableDrivers(); // Enables the MOSFET drivers for both M1 and M2.
+    void disableM1Driver(); // Puts the MOSFET driver for M1 into sleep mode.
+    void disableM2Driver(); // Puts the MOSFET driver for M2 into sleep mode.
+    void disableDrivers(); // Puts the MOSFET drivers for both M1 and M2 into sleep mode.
+    unsigned int getM1CurrentReading();
+    unsigned int getM2CurrentReading();
+    void calibrateM1CurrentOffset();
+    void calibrateM2CurrentOffset();
+    void calibrateCurrentOffsets();
+
   private:
     static const unsigned char _M1PWM = 9;
-	static const unsigned char _M2PWM = 10;
-	unsigned char _M1nSLEEP;
+    static const unsigned char _M2PWM = 10;
+    unsigned char _M1nSLEEP;
     unsigned char _M2nSLEEP;
     unsigned char _M1DIR;
     unsigned char _M2DIR;
     unsigned char _M1nFAULT;
-	unsigned char _M2nFAULT;
+    unsigned char _M2nFAULT;
     unsigned char _M1CS;
-	unsigned char _M2CS;
-	static boolean _flipM1;
+    unsigned char _M2CS;
+    static boolean _flipM1;
     static boolean _flipM2;
-	unsigned int _offsetM1 = 0;
-	unsigned int _offsetM2 = 0;
-    
+    static unsigned int _offsetM1;
+    static unsigned int _offsetM2;
+
 };
 
 class DualG2HighPowerMotorShield18v : public DualG2HighPowerMotorShield
 {
   public:
-	DualG2HighPowerMotorShield18v(): DualG2HighPowerMotorShield() {}
-    DualG2HighPowerMotorShield18v(unsigned char M1nSLEEP, unsigned char M1DIR, unsigned char M1nFAULT, unsigned char M1CS, 
-                        unsigned char M2nSLEEP, unsigned char M2DIR, unsigned char M2nFAULT, unsigned char M2CS) : 
-						DualG2HighPowerMotorShield(M1nSLEEP, M1DIR, M1nFAULT, M1CS, M2nSLEEP, M2DIR, M2nFAULT, M2CS) {}
+    DualG2HighPowerMotorShield18v(): DualG2HighPowerMotorShield() {}
+    DualG2HighPowerMotorShield18v(unsigned char M1nSLEEP, unsigned char M1DIR, unsigned char M1nFAULT, unsigned char M1CS,
+                                  unsigned char M2nSLEEP, unsigned char M2DIR, unsigned char M2nFAULT, unsigned char M2CS) :
+      DualG2HighPowerMotorShield(M1nSLEEP, M1DIR, M1nFAULT, M1CS, M2nSLEEP, M2DIR, M2nFAULT, M2CS) {}
 
-    unsigned int getM1CurrentMilliamps(); // Get current reading for M1. 
+    unsigned int getM1CurrentMilliamps(); // Get current reading for M1.
     unsigned int getM2CurrentMilliamps(); // Get current reading for M2.
 };
 
 class DualG2HighPowerMotorShield24v : public DualG2HighPowerMotorShield
 {
   public:
-	DualG2HighPowerMotorShield24v() : DualG2HighPowerMotorShield() {}
-    DualG2HighPowerMotorShield24v(unsigned char M1nSLEEP, unsigned char M1DIR, unsigned char M1nFAULT, unsigned char M1CS, 
-                        unsigned char M2nSLEEP, unsigned char M2DIR, unsigned char M2nFAULT, unsigned char M2CS) : 
-						DualG2HighPowerMotorShield(M1nSLEEP, M1DIR, M1nFAULT, M1CS, M2nSLEEP, M2DIR, M2nFAULT, M2CS) {}
+    DualG2HighPowerMotorShield24v() : DualG2HighPowerMotorShield() {}
+    DualG2HighPowerMotorShield24v(unsigned char M1nSLEEP, unsigned char M1DIR, unsigned char M1nFAULT, unsigned char M1CS,
+                                  unsigned char M2nSLEEP, unsigned char M2DIR, unsigned char M2nFAULT, unsigned char M2CS) :
+      DualG2HighPowerMotorShield(M1nSLEEP, M1DIR, M1nFAULT, M1CS, M2nSLEEP, M2DIR, M2nFAULT, M2CS) {}
 
-    unsigned int getM1CurrentMilliamps(); // Get current reading for M1. 
+    unsigned int getM1CurrentMilliamps(); // Get current reading for M1.
     unsigned int getM2CurrentMilliamps(); // Get current reading for M2.
 };
 
